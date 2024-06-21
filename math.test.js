71587350a48cee88a5d25960520dd8c692ad1ea9
Rezak-Khorsi <rz.khorsi@gmail.com>
2024-06-21 14:52:30 +0200

    Update math.test.js
    rajout de "dd" pour générer une erreur

--- math.test.js
+++ math.test.js
@@ -2,7 +2,7 @@
 const add = require('./math');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(3);dd
 });
 
 test('adds -1 + 1 to equal 0', () => {
