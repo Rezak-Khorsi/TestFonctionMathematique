b21d029111b096b796450157620d90c8adb80e22
Rezak-Khorsi <rz.khorsi@gmail.com>
2024-06-21 14:59:13 +0200

    Update math.test.js
    3ème erreur

--- math.test.js
+++ math.test.js
@@ -2,7 +2,7 @@
 const add = require('./math');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);dddd
+    expect(add(1, 2)).toBe(3);dddddsd
 });
 
 test('adds -1 + 1 to equal 0', () => {
