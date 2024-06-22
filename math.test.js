8873b8df6043e585197d85a1a97a1ec0252a2504
Rezak <rz.khorsi@gmail.com>
2024-06-22 09:38:18 +0200

    voir si Jenkins detecte la modif

--- math.test.js
+++ math.test.js
@@ -3,6 +3,7 @@ const add = require('./math');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
+    // rajout de commentaire pour voir si Jenkins détecte la modif
 });
 
 test('adds -1 + 1 to equal 0', () => {
